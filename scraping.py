04d6637d79ee700e3f7961c99279682a0c3ee74f
GimaYuto <yurihanagima@gmail.com>
2022-04-24 22:04:22 +0900

    沖縄のトップニュース取得

--- scraping.py
+++ scraping.py
@@ -6,8 +6,11 @@ driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
 
 driver.get('https://www.google.co.jp/')
 search_bar = driver.find_element_by_name("q")
-search_bar.send_keys("python")
+search_bar.send_keys("沖縄")
 search_bar.submit()
+
+for news in driver.find_elements_by_class_name('mCBkyc'):
+    print(news.text)
 
 time.sleep(10)
 print("finish")
