7a6674278a15fa66aa2e6bcf65f2dad1de9988d4
GimaYuto <yurihanagima@gmail.com>
2022-04-26 22:27:42 +0900

    Twitterにログインしてツイート

---
+++ tweet.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import chromedriver_binary
+import time
+
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+
+account = ""
+password = ""
+text ="seleniumでログインしてツイートしています。"
+
+driver.get('https://twitter.com/login/')
+time.sleep(3)
+loginUser = driver.find_element_by_name("text")
+loginUser.send_keys(account)
+time.sleep(3)
+nextButton = driver.find_elements_by_class_name("r-usiww2")
+nextButton[2].click()
+time.sleep(3)
+loginPassword = driver.find_element_by_name("password")
+loginPassword.send_keys(password)
+time.sleep(3)
+loginButton = driver.find_elements_by_class_name("r-sdzlij")
+loginButton[2].click()
+time.sleep(3)
+tweetText = driver.find_element_by_class_name("notranslate")
+tweetText.click()
+tweetText.send_keys(text)
+time.sleep(1)
+tweetButton = driver.find_elements_by_class_name("r-19u6a5r")
+tweetButton[0].click()
+
+time.sleep(10)
+print("finish")
+driver.quit()
